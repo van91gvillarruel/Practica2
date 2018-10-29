889242406cd07b6ba4f2458e81f71190362196ad
diego <diego.acj23@gmail.com>
2018-10-29 12:00:11 -0700

    Configuracion app jugador

--- jugador/views.py
+++ jugador/views.py
@@ -1,8 +1,11 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
-from rest_framework.response import Repsonse
+from rest_framework.response import Response
 from .model import Jugador
 
 class JugadorViewSet(viewsets.ModelViewSet):
+    queryset = Jugador.objects.all()
 
-
+    def list(self, request):
+        print('servicio get')
+        return Response('ok')
--- practica2/urls.py
+++ practica2/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from rest_frameworks.routes import DefaultRouter
+from rest_framework.routes import DefaultRouter
 from jugador.views import JugadorViewSet
 
 router = DefaultRouter()
