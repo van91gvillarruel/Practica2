5701f64509c199772dd8bb8a2b2e8d71f32c89c2
diego <diego.acj23@gmail.com>
2018-10-30 09:02:40 -0700

    modelo creado

--- jugador/views.py
+++ jugador/views.py
@@ -1,7 +1,12 @@
 from rest_framework import viewsets
 from .serializers import JugadorSerializer
 from .models import Jugador
+from rest_framework.response import Response
+
 
 class JugadorViewSet(viewsets.ModelViewSet):
     serializer_class = JugadorSerializer
     queryset = Jugador.objects.all()
+
+
+
