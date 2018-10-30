e6dcc06f5cdbbed9f59b7ff201fe10b30e22d38a
18dcjuarez <dcjuarez18@outlook.com>
2018-10-30 10:12:49 -0600

    Creacion de modelo competencias

--- competencia/models.py
+++ competencia/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Competencia(models.Model):
+    # player_one = models.ForeignKey(null=False)
+    player_one_score = models.IntegerField(null=False)
+    # player_two = models.ForeignKey(null=False)
+    player_two_score = models.IntegerField(null=False)
+    played_time = models.DateTimeField(auto_now_add=True)
