8c948405d147670f0bc982915c14fdbe3479f3b2
18dcjuarez <dcjuarez18@outlook.com>
2018-10-31 09:46:42 -0600

    validar que existan jugadores

--- competencia/serializers.py
+++ competencia/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 from .models import Competencia
 from jugador.models import Jugador
+from jugador.serializers import JugadorSerializer
 
 
 class CompetenciaSerializer(serializers.ModelSerializer):
@@ -10,19 +11,43 @@ class CompetenciaSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class CrearCompetenciaSerializer(serializers.ModelSerializer):
+class CrearCompetenciaSerializer(serializers.Serializer):
     player_one = serializers.IntegerField()
     player_two = serializers.IntegerField()
-
-    class Meta:
-        model = Jugador
-        fields = '__all__'
 
     def crear(self):
         print("Estoy en Crear")
         return {"ok"}
 
     def validate_player_one(self, param):
-        print("Estoy en validate")
-        return {"ok"}
+        print("param validate 1: ")
+        print(param)
+        print(self.context)
+        print("Estoy en validate 1")
+        if Jugador.objects.filter(id=param):
+            print(param)
+            return param
+        else:
+            raise serializers.ValidationError("Jugador uno no existe")
+
+    def validate_player_two(self, param):
+        print(self.context)
+        print("Estoy en validate 2")
+        if Jugador.objects.filter(id=param):
+            return param
+        else:
+            raise serializers.ValidationError("Jugador dos no existe")
+
+    def validate(self, param):
+        print(self.context)
+        print("Estoy en validate 3")
+        print(self.context)
+        print(self.context['player_one'])
+        p1 = Jugador.objects.filter(id=self.context['player_one'])
+        p2 = Jugador.objects.filter(id=self.context['player_two'])
+        print(JugadorSerializer(p1, many=True).data)
+        print(JugadorSerializer(p2, many=True).data)
+
+        return {'ok'}
+
 
