6819e2a13cc6f63b16768173792b49820a206a8b
18dcjuarez <dcjuarez18@outlook.com>
2018-10-31 11:14:32 -0600

    Validate for competition

--- competencia/serializers.py
+++ competencia/serializers.py
@@ -2,11 +2,8 @@ from rest_framework import serializers
 
 from .models import Competencia
 from jugador.models import Jugador
-from jugador.serializers import JugadorSerializer
-from team.models import Team
 
 from random import randint
-
 
 
 class CompetenciaSerializer(serializers.ModelSerializer):
@@ -27,7 +24,7 @@ class CrearCompetenciaSerializer(serializers.Serializer):
         competencia = Competencia()
 
         player_one = randint(0, 5)
-        player_two = randint(0,5)
+        player_two = randint(0, 5)
 
         if (player_one>player_two):
             print("gano jugador 1")
@@ -41,34 +38,23 @@ class CrearCompetenciaSerializer(serializers.Serializer):
 
          # competencia.save()
 
-
     def validate_player_one(self, param):
-        print("param validate 1: ")
-        print(param)
-        print(self.context)
-        print("Estoy en validate 1")
         if Jugador.objects.filter(id=param):
-            print(param)
             return param
         else:
             raise serializers.ValidationError("Jugador uno no existe")
 
     def validate_player_two(self, param):
-        print(self.context)
-        print("Estoy en validate 2")
         if Jugador.objects.filter(id=param):
             return param
         else:
             raise serializers.ValidationError("Jugador dos no existe")
 
     def validate(self, param):
-        print(self.context)
-        print("Estoy en validate 3")
-        print(self.context)
-        print(self.context['player_one'])
-        p1 = Jugador.objects.filter(id=self.context['player_one'])
-        p2 = Jugador.objects.filter(id=self.context['player_two'])
-        print(JugadorSerializer(p1, many=True).data)
-        print(JugadorSerializer(p2, many=True).data)
+        p1 = Jugador.objects.filter(id=self.context['player_one']).first()
+        p2 = Jugador.objects.filter(id=self.context['player_two']).first()
+        if p1.team.id != p2.team.id:
+            return param
+        else:
+            raise serializers.ValidationError("Jugadores pertenecen al mismo equipo")
 
-        return {'ok'}
